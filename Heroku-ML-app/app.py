081786c3f37343187b3d251cea0478b9d73bfe4f
Sourcery AI <>
2023-12-02 22:26:42 +0000

    'Refactored by Sourcery'

--- Heroku-ML-app/app.py
+++ Heroku-ML-app/app.py
@@ -19,15 +19,13 @@ def predict():
     feature_list = list(feature_list.values())
     feature_list = list(map(int, feature_list))
     final_features = np.array(feature_list).reshape(1, 12) 
-    
+
     prediction = model.predict(final_features)
     output = int(prediction[0])
-    if output == 1:
-        text = ">50K"
-    else:
-        text = "<=50K"
-
-    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
+    text = ">50K" if output == 1 else "<=50K"
+    return render_template(
+        'index.html', prediction_text=f'Employee Income is {text}'
+    )
 
 
 if __name__ == "__main__":
